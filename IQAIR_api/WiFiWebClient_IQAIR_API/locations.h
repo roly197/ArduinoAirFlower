2cfb1dd0b0813a2b6641182cac70e05e0ba5bb1d
roly197 <roly.van.leersum@ingbank.com>
2025-01-05 15:48:38 +0100

    Update locations.h
    added vacuum pump and dual action programs

--- IQAIR_api/WiFiWebClient_IQAIR_API/locations.h
+++ IQAIR_api/WiFiWebClient_IQAIR_API/locations.h
@@ -9,12 +9,22 @@ const String Location[NumberOfLocations][3] = {
                          // City, Region , Country 
                          
 const int NumberOfIntervals = 3;                         
-const int AirIntervals[NumberOfLocations][7] = {
-                          {0,100,20000,6000,2000,6000,2000},
-                          {101,200,20000,3000,1000,3000,1000},
-                          {201,999,20000,1000,0000,1000,0000}
+const int AirIntervals[NumberOfLocations][8] = {
+                          {0,100,20000,20000,6000,2000,6000,2000},
+                          {101,200,20000,20000,3000,1000,3000,1000},
+                          {201,999,20000,20000,1000,0000,1000,0000}
                           };
-                         // MinResult, MaxResult, preset(ms), inAir(ms), pause1(ms), outAir(ms), pause2(ms)
+                         // MinResult, MaxResult, presetDeflate(ms), presetInflate(ms), inAir(ms), pause1(ms), outAir(ms), pause2(ms)
+
+
+const bool VacuumValveOfPump = false; //Set to "true" to use valve and "false" to deflate with vacuum pump.
+const bool DualAction = false; // Set to true to use dual pump and dual valves. 
+// Program | VacuumValveOfPump | DualAction
+// --------|-------------------|-----------
+//    1    |       true        |   false   (single motor, single valve)
+//    2    |       false       |   false   (single motor, vacuum motor)
+//    3    |       true        |   true    (dual motor, dual valve)
+//   n.a.  |       false       |   true    (cannot use pump2 to inflate and deflate at the same time) 
 
 // From here: do not change variables unless you know what you're doing :-)
 
